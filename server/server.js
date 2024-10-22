7f7f59573c7afc5fd90fee7bf8c28ce558ec6af8
aliraihann <aliraihann@gmail.com>
2024-10-22 11:41:59 +0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dbConfig from './config/dbConfig.js'
 import { configDotenv } from "dotenv";
 configDotenv();
 import portfolioRoutes from './routes/portfolioRoute.js';
@@ -10,13 +9,7 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoutes);
 
 const port = process.env.PORT || 4000;
-/* For Deployment */
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "client/build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build/index.html"))
-    })
-}
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
