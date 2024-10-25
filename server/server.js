6fbb17099b958a094557adf0f2b29bd88d9dbc96
aliraihann <aliraihann@gmail.com>
2024-10-25 14:06:13 +0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 import express from 'express';
+import dbConfig from './config/dbConfig.js'
 import { configDotenv } from "dotenv";
 configDotenv();
 import portfolioRoutes from './routes/portfolioRoute.js';
-import path from 'path';
-
+import cors from "cors"
 const app = express();
+
+
 app.use(express.json());
+app.use(cors());
+
+// server routes
 app.use("/api/portfolio", portfolioRoutes);
 
 const port = process.env.PORT || 4000;
 
+// app listen for the server running
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
